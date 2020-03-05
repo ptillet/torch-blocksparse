4ad62f4ab7a8b0d77b2a7c54236906e19d7a4e80
Philippe Tillet <ptillet@g.harvard.edu>
2020-03-05 16:42:01 -0500

    [TEST] Cleaned test code

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import torch_blocksparse
 import torch
+torch.manual_seed(0)
 
 def reference_dot(x, w, mask):
   WS0, WS1 = w.size()
@@ -16,55 +17,44 @@ def reference_dot(x, w, mask):
               wj : wj+block_size_1] *= mask[bi, bj]
   return torch.matmul(x, maskedw)
 
-torch.manual_seed(0)
+def run_reference(x, w, mask, bsz, dy):
+  y = reference_dot(x, w, mask)
+  y.backward(dy)
+  dx = x.grad.clone()
+  dw = w.grad.clone()
+  x.grad.zero_()
+  w.grad.zero_()
+  return y, dx, dw
+
+def run_triton(x, w, mask, bsz, dy):
+  linear = torch_blocksparse.Linear(x.size(0), x.size(1), bsz, mask).cuda()
+  linear.weight.data.copy_(w)
+  y = linear(x)
+  y.backward(dy)
+  dx = x.grad.clone()
+  dw = linear.weight.grad.clone()
+  x.grad.zero_()
+  w.grad.zero_()
+  return y, dx, dw
+
+
+
+
 # parameters
 M, N, K = 256, 256, 256
-BS = 16
+bsz = 16
 # initialize inputs
-mask = torch.randint(0, 2, (K//BS, N//BS))
+mask = torch.randint(0, 2, (K//bsz, N//bsz))
 x = torch.rand((M, K), dtype=torch.float32, requires_grad=True).cuda()
 w = torch.rand((K, N), dtype=torch.float32, requires_grad=True).cuda()
+dy = torch.rand((M, N), dtype=torch.float32).cuda()
 x.retain_grad()
 w.retain_grad()
-# reference result
-ry = reference_dot(x, w, mask)
-dy = torch.rand_like(ry)
-ry.backward(dy)
-rdx = x.grad.clone()
-rdw = w.grad.clone()
-# reset gradients
-x.grad.zero_()
-w.grad.zero_()
-# triton result
-y_lut, y_locks, y_width = torch_blocksparse._linear.make_ydx_lut(mask, BS)
-dx_lut, dx_locks, dx_width = torch_blocksparse._linear.make_ydx_lut(mask.T, BS)
-dw_lut, dw_locks, dw_width = torch_blocksparse._linear.make_dw_lut(mask, BS)
-ty = torch_blocksparse._linear.apply(x, w, BS, 
-                               y_lut, y_locks, y_width,
-                               dx_lut, dx_locks, dx_width,
-                               dw_lut, dw_locks, dw_width)
-ty.backward(dy)
-tdx = x.grad.clone()
-tdw = w.grad.clone()
-x.grad.zero_()
-w.grad.zero_()
+# run
+ry, rdx, rdw = run_reference(x, w, mask, bsz, dy)
+ty, tdx, tdw = run_triton(x, w, mask, bsz, dy)
 # test
-print((ty - ry).abs().max())
-print((tdx - rdx).abs().max())
-print((tdw - rdw).abs().max())
-
-
-###############
-# Test Module #
-###############
-
-linear = torch_blocksparse.Linear(K, N, BS, mask).cuda()
-linear.weight.data.copy_(w)
-nny = linear(x)
-nny.backward(dy)
-nndx = x.grad.clone()
-nndw = linear.weight.grad.clone()
-# test
-print((nny - ry).abs().max())
-print((nndx - rdx).abs().max())
-print((nndw - rdw).abs().max())
+assert(torch.allclose(ty, ry))
+assert(torch.allclose(tdx, rdx))
+assert(torch.allclose(tdw, rdw))
+# benchmark
