[CORE] Fixed memory issue in Softmax

tests/seq2seq.py
@@ -89,13 +89,13 @@ def batchify(data, bsz):
     data = data.view(bsz, -1).t().contiguous()
     return data.to(device)
 
-batch_size = 32
-eval_batch_size = 10
+batch_size = 4
+eval_batch_size = 4
 train_data = batchify(train_txt, batch_size)
 val_data = batchify(val_txt, eval_batch_size)
 test_data = batchify(test_txt, eval_batch_size)
 
-bptt = 128
+bptt = 1024
 def get_batch(source, i):
     seq_len = min(bptt, len(source) - 1 - i)
     data = source[i:i+seq_len]
@@ -107,12 +107,13 @@ def get_batch(source, i):
 ntokens = len(TEXT.vocab.stoi) # the size of vocabulary
 emsize = 768 # embedding dimension
 nhid = 768 # the dimension of the feedforward network model in nn.TransformerEncoder
-nlayers = 8 # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+nlayers = 4 # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
 nhead = 12 # the number of heads in the multiheadattention models
 dropout = 0. # the dropout value
 torch.manual_seed(0)
 model = TransformerModel(ntokens, emsize, nhead, nhid, nlayers, dropout).to(device)
-#torch_blocksparse.replace_mha(model)
+info = torch_blocksparse.MultiheadAttention.SparsityInfo(mode='fixed', block=32, numverts=4, vertsize=1)
+torch_blocksparse.replace_mha(model, info)
 torch.manual_seed(0)
 
 # Run the model
@@ -141,10 +142,10 @@ def train():
             cur_loss = total_loss / log_interval
             elapsed = time.time() - start_time
             print('| epoch {:3d} | {:5d}/{:5d} batches | '
-                  'lr {:02.2f} | ms/batch {:5.2f}'
+                  'lr {:02.2f} | ms/batch {:5.2f} | memory {:5.2f}GB | '
                   'loss {:5.2f} | ppl {:8.2f}'.format(
                     epoch, batch, len(train_data) // bptt, scheduler.get_lr()[0],
-                    elapsed * 1000 / log_interval,
+                    elapsed * 1000 / log_interval, torch.cuda.memory_allocated()*1e-9,
                     cur_loss, math.exp(cur_loss)))
             total_loss = 0
             start_time = time.time()

torch_blocksparse/matmul.py
@@ -206,7 +206,7 @@ class _sparse_matmul(torch.autograd.Function):
     # https://github.com/openai/blocksparse/blob/master/blocksparse/matmul.py#L95
     max_size = sizes.max()
     min_size = sizes[torch.nonzero(sizes)].min()
-    if max_size > min_size > 2.0:
+    if max_size > min_size * 2.0:
       seg_max = max(triton.cdiv(max_size, 4), min_size*2)
     else:
       seg_max = max_size

torch_blocksparse/softmax.py
@@ -186,8 +186,8 @@ class _sparse_softmax(torch.autograd.Function):
         # run kernel
         kernel = _sparse_softmax.make_kernel(fwd_kernels, fwd_src, maxlut*block, x.dtype, block, 
                                              kp_mask_mode, attn_mask_mode)
-        grid = lambda opt: [triton.cdiv(spdims[0] * spdims[1] * block, opt.d('TM')),
-                            x.shape[0]]
+        M = x.shape[0]
+        grid = lambda opt: [triton.cdiv(spdims[0] * spdims[1] * block, opt.d('TM')), M]
         # handle None key_padding_mask
         stride_zkpm = 0 if key_padding_mask is None else key_padding_mask.stride(0)
         key_padding_mask = torch.empty(0, dtype=x.dtype, device=x.device) if key_padding_mask is None else key_padding_mask
@@ -217,8 +217,8 @@ class _sparse_softmax(torch.autograd.Function):
         # run kernel
         kernel = _sparse_softmax.make_kernel(bwd_kernels, bwd_src, ctx.maxlut*ctx.block, x.dtype, ctx.block, 
                                              ctx.kp_mask_mode, ctx.attn_mask_mode)
-        grid = lambda opt: [triton.cdiv(ctx.spdims[0] * ctx.spdims[1] * ctx.block, opt.d('TM')),
-                            x.shape[0]]
+        M = x.shape[0]
+        grid = lambda opt: [triton.cdiv(ctx.spdims[0] * ctx.spdims[1] * ctx.block, opt.d('TM')), M]
         kernel(x, ctx.scale, dx, lut, ctx.maxlut, x.stride(0), dx.stride(0), grid=grid)
         return dx, None, None, None, None, None, None, None, None, None, None, None, None
 