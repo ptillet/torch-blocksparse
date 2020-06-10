7d71754eac92c6e261494c1c3bbb5d433ce6aed9
arashashari <arash.ashari@gmail.com>
2020-06-10 20:57:50 +0000

    updating init file with new module name

--- torch_blocksparse/__init__.py
+++ torch_blocksparse/__init__.py
@@ -2,7 +2,7 @@ from .softmax import Softmax
 from .matmul import MatMul, Linear
 from .conv import _sparse_conv2d, Conv2d
 from .attention import MultiheadAttention
-from .simplifiedsparseattention import SimplifiedSparseAttention
+from .deepspeedsparseselfattention import DeepSpeedSparseSelfAttention
 from .batchnorm import BatchNorm2d
 from .permute import _permute, Permute
 from .relu import ReLU
